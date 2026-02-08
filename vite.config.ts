e79586aa9b734e6b32500a264d5f537b79f28347
Repositórios de Ediel <pensamentologicoed@gmail.com>
2026-02-08 19:40:41 -0300

    Atualizando

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Garante que o process.env.API_KEY seja acessível durante o build e runtime
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
-    target: 'esnext',
     outDir: 'dist',
+    target: 'esnext',
     rollupOptions: {
       input: {
-        main: 'index.html',
-        privacy: 'privacy.html'
+        main: './index.html',
+        privacy: './privacy.html'
       }
     }
   }
